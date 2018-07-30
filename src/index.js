d3743be3462f49ab62b04412daeccc1dc0a3f158
adamgajzlerowicz <nelf86@gmail.com>
2018-07-30 14:44:08 +0200

    added base tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // @flow
 
-console.log('lkjsd');
+const foo = (data)=>data;
+
+export {
+    foo,
+};
---
+++ src/index.spec.js
@@ -0,0 +1,5 @@
+import { foo } from './index';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(foo('a')).toBe('a');
+});
